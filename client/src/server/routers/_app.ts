0e6327f1c0a18782cbddb084d0143a967a46adbc
Ido Pesok <idopesok91@gmail.com>
2023-04-19 09:42:29 -0700

    next js trpc setup

---
+++ client/src/server/routers/_app.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+import { publicProcedure, router } from '../trpc';
+
+export const appRouter = router({
+  hello: publicProcedure
+    .input(
+      z.object({
+        text: z.string(),
+      }),
+    )
+    .query(async (opts) => {
+      const { input } = opts;
+      return {
+        greeting: `hello ${input.text}`,
+      };
+    }),
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
