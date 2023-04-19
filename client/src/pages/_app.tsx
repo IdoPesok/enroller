13820320c749d16c71eff6e5e3d7e764f9bf877b
Ido Pesok <idopesok91@gmail.com>
2023-04-19 02:15:12 -0700

    added clerk

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import { Toaster } from '@/components/ui/toaster'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ClerkProvider } from '@clerk/nextjs';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ClerkProvider>
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </ClerkProvider>
   )
 }
