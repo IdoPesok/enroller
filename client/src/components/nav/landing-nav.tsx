192888436389ce6ff6dcb8c1d4fdf5b25f2d95c5
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2023-04-26 17:35:58 -0700

    clean out unneeded reactive code in landing nav

--- client/src/components/nav/landing-nav.tsx
+++ client/src/components/nav/landing-nav.tsx
@@ -4,10 +4,6 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"
 import { AcademicCapIcon } from "@heroicons/react/24/solid"
 import { SignInButton, useAuth, UserButton } from "@clerk/clerk-react"
 
-const navigation = [
-  { name: "Salmon Labs", href: "/" },
-]
-
 type Props = {
   children: ReactNode
 }
@@ -41,15 +37,9 @@ export const LandingNav = ({ children }: Props) => {
               </button>
             </div>
             <div className="hidden lg:flex lg:gap-x-12">
-              {navigation.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className="text-sm font-semibold leading-6 text-gray-900"
-                >
-                  {item.name}
-                </a>
-              ))}
+              <p className="text-sm font-semibold leading-6 text-gray-900">
+                Salmon Labs
+              </p>
             </div>
             <div className="hidden lg:flex lg:flex-1 lg:justify-end">
               {userId ? (
@@ -91,17 +81,6 @@ export const LandingNav = ({ children }: Props) => {
               </div>
               <div className="mt-6 flow-root">
                 <div className="-my-6 divide-y divide-gray-500/10">
-                  <div className="space-y-2 py-6">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
                   <div className="py-6">
                     <a
                       href="#"
