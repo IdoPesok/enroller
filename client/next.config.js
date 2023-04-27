529451da91c4aeeb0928ec264d444a6027c87556
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2023-04-26 17:36:17 -0700

    fix monorepo problems

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  typescript: {
-    /* jank hack because trpc is currently failing type checking no idea why */
-    ignoreBuildErrors: true,
-  }
 }
 
 module.exports = nextConfig
--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -2,12 +2,13 @@ export default function Error404() {
   return (
     <>
       <div className="flex justify-center items-center gap-10 py-20 flex-col">
-        <h1
-          className="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-emerald-400 to-green-600"
-        >
+        <h1 className="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-emerald-400 to-green-600">
           404
         </h1>
-        <p>You didn't break the internet, but we can't find what you are looking for.</p>
+        <p>
+          You didn&apos;t break the internet, but we can&apos;t find what you
+          are looking for.
+        </p>
       </div>
     </>
   )
