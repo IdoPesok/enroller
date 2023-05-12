f640976af95ab19f970dca01c4b9eb784eb667da
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2023-05-11 20:34:30 -0700

    tweak spacing

--- client/src/components/courses/data-table.tsx
+++ client/src/components/courses/data-table.tsx
@@ -67,7 +67,7 @@ export function DataTable<TData, TValue>({
   })
 
   return (
-    <div className="space-y-4">
+    <div className="space-y-2">
       <DataTableToolbar table={table} />
       <div className="rounded-md border">
         <Table>
