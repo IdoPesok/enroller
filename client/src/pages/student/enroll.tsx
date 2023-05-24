f279ddaa226021d3ac3423a63509e2b86656a8cc
Sarah Duncan <sduncan1229@gmail.com>
2023-05-23 20:00:57 -0700

    Finishes rough enroll frontend and shopping cart.

--- client/src/pages/student/enroll.tsx
+++ client/src/pages/student/enroll.tsx
@@ -1,8 +1,18 @@
 import { useState } from "react"
 import ShoppingCart from "../../components/enroll/shopping-cart"
+import WeekCalendar from "@/components/WeekCalendar/WeekCalendar"
+
+const CURRENT_QUARTER = 'Spring 2023' // hard coded for now as placeholder
+const calendarHeight = 400 // TODO: find best way of sizing height 
+const calenderWidth = 400
 
 export default function Enroll(){
     return(
-        <ShoppingCart></ShoppingCart>
+        <>
+        <div className = "flex flex-row">
+            <WeekCalendar currentQuarter='' height={calendarHeight} width="w-3/5" courses={[]}></WeekCalendar>
+            <ShoppingCart></ShoppingCart>
+        </div>
+        </>
     );
 }
