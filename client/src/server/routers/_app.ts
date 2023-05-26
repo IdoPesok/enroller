7f5f91cc795b828a1446b8bea099942f0ed7b4f5
Tyler Collins <tylerjcollins2020@gmail.com>
2023-05-25 21:11:09 -0700

    creates home router

--- client/src/server/routers/_app.ts
+++ client/src/server/routers/_app.ts
@@ -3,6 +3,7 @@ import { authRouter } from "./auth"
 import { courseRouter } from "./courses"
 import { degreeProgressRouter } from "./degree-progress"
 import { exploreRouter } from "./explore"
+import { homeRouter } from "./home"
 import { onboardRouter } from "./onboard"
 import { sectionRouter } from "./sections"
 
@@ -12,7 +13,8 @@ export const appRouter = router({
   onboard: onboardRouter,
   auth: authRouter,
   degreeProgress: degreeProgressRouter,
-  section: sectionRouter
+  section: sectionRouter,
+  home: homeRouter
 })
 
 export type AppRouter = typeof appRouter
---
+++ client/src/server/routers/home.ts
@@ -0,0 +1,26 @@
+import { prisma } from "@/server/prisma"
+import { z } from "zod"
+import { studentProcedure, router } from "../trpc"
+import { Enrolled_Type } from "@prisma/client"
+
+// tylerjcollins.csc user id: user_2QDp9b95iUuYTE2TVxBS6cRJK4F
+
+export const homeRouter = router({
+
+    userSections: studentProcedure
+        .input(z.object({ user: z.string(), types: z.array(z.nativeEnum(Enrolled_Type)) }))
+        .query(async ({ input, ctx }) => {
+            const sections = await prisma.enrolled.findMany({
+            where: {
+                User: ctx.auth.userId,
+                Type: { in: input.types }
+            },
+            include: {
+                Section: true
+            }
+            });
+            return sections.map(enrollment => enrollment.Section);
+        })
+})
+
+export type AppRouter = typeof homeRouter
