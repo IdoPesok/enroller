30377e147a1e7590378dc2fc892fc76ca902f212
Sarah Duncan <sduncan1229@gmail.com>
2023-05-27 19:10:05 -0700

    Begins importing data (commit before rebase)

--- client/src/pages/student/enroll.tsx
+++ client/src/pages/student/enroll.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react"
 import ShoppingCart from "../../components/enroll/shopping-cart"
 import WeekCalendar from "@/components/WeekCalendar/WeekCalendar"
+import { Sections } from "@prisma/client"
+import { Enrolled } from "@prisma/client"
+
 
 const CURRENT_QUARTER = 'Spring 2023' // hard coded for now as placeholder
 const calendarHeight = 400 // TODO: find best way of sizing height 
